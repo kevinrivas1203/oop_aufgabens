51a1eddacf7bb446cf3a0afe2cf9a7998086a5c7
kevinrivas1203 <kevin_rivas@teilnehmer.cdemy.de>
2026-05-11 13:42:23 +0200

    oop1 finisch

--- oop1/bin/main.dart
+++ oop1/bin/main.dart
@@ -1,3 +1,19 @@
-import 'package:oop1/teilnehmer.dart' as oop1;
+import 'package:oop1/teilnehmer.dart';
 
-void main() {}
+void main() {
+  final a = Teilnehmer(lastName: 'Rivas', firtsName: 'Kevin');
+  print('a.firstName : ${a.firtsName}');
+  print('a.age : ${a.age}');
+
+  final b = Teilnehmer(
+    lastName: 'Santiago',
+    firtsName: 'Gonzales',
+    age: 30,
+    adrresse: 'Barcelona',
+  );
+  print('B.firstName : ${b.firtsName}');
+  print('B.lastName : ${b.lastName}');
+  print('B.age : ${b.age}');
+  b.age = 35;
+  print('B.age : ${b.age}');
+}
